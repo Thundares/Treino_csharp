c396356ded6b9b2296557b9f33ee65e4b57b26a3
Rafael Mippo <rkmippo@gmail.com>
2019-09-29 14:17:05 -0300

    Primeiro exercícios de MVC iniciado

---
+++ MVC_02/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exercicios.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
